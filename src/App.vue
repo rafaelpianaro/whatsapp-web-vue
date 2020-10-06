125c28de432d1b71c09041bb468edb990c1b3a88
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-05 22:25:41 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <!-- esquerda -->
+          <div class="column is-3 lista-de-conversas">
+            <div class="barra-superior" />
+            <div class="item" v-for="conversa in conversas" :key="conversa">
+              <div class="title is-6">
+                {{ conversa.usuario }}
+              </div>
+              <div class="subtitle is-6">
+                Última Mensagem...
+              </div>
+            </div>
+          </div>
+          <!-- direita -->
+          <div class="column conversa-ativa">
+            <div class="barra-superior">
+              <span>Usuário</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import conversasIniciais from './dados'
+export default {
+  data(){
+    return {
+      conversas: conversasIniciais
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .columns{
+    min-height: 800px;
+    box-shadow: 0 3rem -1rem rgba(10,10,10,.2);
+  }
+
+  .lista-de-conversas{
+    background: white;
+  }
+  .conversa-ativa{
+    background: #E5DDD5;
+  }
+</style>
